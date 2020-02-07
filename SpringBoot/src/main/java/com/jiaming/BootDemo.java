88126cdccd31110950000e0d04213d0cf36e6f73
tom.glb <goodMorning_glb@jiaming.com>
2020-02-07 10:49:06 +0800

    SpringBoot注入

--- SpringBoot/src/main/java/com/jiaming/BootDemo.java
+++ SpringBoot/src/main/java/com/jiaming/BootDemo.java
@@ -2,12 +2,15 @@ package com.jiaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author jmstart
  * @create 2020-02-06 10:36
  * 启动 SpringBoot
- * 这个类要放在二级包下
+ * 这个类要放在二级包下为什么？
+ *  因为没有写 @ComponentScan 扫描器
+ *  所以它底层默认扫描子包
  */
 
 @SpringBootApplication
--- SpringBoot/src/main/java/com/jiaming/config/JdbcConfig.java
+++ SpringBoot/src/main/java/com/jiaming/config/JdbcConfig.java
@@ -1,7 +1,10 @@
 package com.jiaming.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -19,27 +22,52 @@ import javax.sql.DataSource;
  * - @PropertySource：指定外部属性文件，
  */
 @Configuration
-@PropertySource("classpath:jdbc.properties")
+//@PropertySource("classpath:application.properties")
+//@EnableConfigurationProperties(JdbcProperties.class)
 public class JdbcConfig {
 
-    @Value("${jdbc.url}")
+    //Java配置方式
+    /*@Value("${jdbc.url}")
     String url;
     @Value("${jdbc.driverClassName}")
     String driverClassName;
     @Value("${jdbc.username}")
     String username;
     @Value("${jdbc.password}")
-    String password;
+    String password;*/
 
-    @Bean
-    public DataSource dataSource(){
+   /*
+    //SpringBoot 属性注入方式一
+    // 偏向于大家一起使用 哪里需要去哪里
+    //1.@Autowired注入使用 2.还可以构造方法注入 3.@Bean注入使用
+    @Autowired                          //1
+    JdbcProperties jdbcProperties;
+
+    public JdbcConfig(JdbcProperties jdbcProperties){        //2
+        this.jdbcProperties = jdbcProperties;
+    }*/
+
+   /* @Bean                                     //3
+    public DataSource dataSource(JdbcProperties prop){
 
         DruidDataSource dataSource = new DruidDataSource();
 
-        dataSource.setUrl(url);
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        //prop.getUrl()它们报红原因可能是Lombok没有起作用 下载了一个Lombok插件就好用了
+        dataSource.setUrl(prop.getUrl());
+        dataSource.setDriverClassName(prop.getDriverClassName());
+        dataSource.setUsername(prop.getUsername());
+        dataSource.setPassword(prop.getPassword());
         return dataSource;
+    }*/
+
+
+    //SpringBoot 属性注入方式二
+    // 偏向于自己使用 更加方便快捷
+    // 直接注入
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource(){
+
+        return new DruidDataSource();
     }
 }
